26f749d0f13e795d3793da79fb67a49d59d0e91b
iplatos <platonoff9168@gmail.com>
2022-05-23 15:56:56 +0300

    react-kabzda

---
+++ src/Tehnologies.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const Tehnologies = () =>{
+    return (<div>
+    <ul>
+      <li>rweq</li>
+      <li>rwe</li>
+      <li>we</li>
+      <li>er</li>
+    </ul>
+  </div>)
+  }
+  export default Tehnologies;
